06aca3434d9bd570625209c06b85c2834683359e
Eddy Salzmann <hello@eddysalzmann.com>
2020-11-25 13:45:31 +0100

    Update ContentView.swift

--- Swift 3D Knob/ContentView.swift
+++ Swift 3D Knob/ContentView.swift
@@ -11,43 +11,34 @@ struct ContentView: View {
 
     var body: some View {
         VStack{
-            ImageButton(start: 0, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 0.5)
+            Swift3DButton(start: 0, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 0.5)
             Text("Speed 0.5")
-            ImageButton(start: 60, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 1)
+            Swift3DButton(start: 60, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 1)
             Text("Speed 1")
-            ImageButton(start: 104, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 1.5)
+            Swift3DButton(start: 104, width: 200, height: 200, minVal: 0, maxVal: 127, speed: 1.5)
             Text("Speed 1.5")
         }
     }
 }
 
-struct ImageButton : View{
+struct Swift3DButton : View{
     
+    // Init values
     var start: Int
     var width: CGFloat
     var height: CGFloat
     var minVal: Int
     var maxVal: Int
     var speed: Double
-
+    
+    // Global values
     @State private var index = 0
     @State private var lastX : CGFloat = 0
     @State private var lastY : CGFloat = 0
     
     private let images = (0...127).map { UIImage(named: "knob_\($0).jpg")! }
-
-    var body: some View {
-        Image(uiImage: images[index])
-            .resizable()
-            .frame(width: width, height: height, alignment: .center)
-            .gesture(drag)
-            .onAppear{
-                if start <= maxVal && start >= minVal {
-                    index = start
-                }
-            }
-    }
     
+    // Handle drag rotate
     var drag: some Gesture {
         DragGesture()
             .onChanged {value in
@@ -82,6 +73,21 @@ struct ImageButton : View{
             .onEnded { value in
                 lastX = 0
                 lastY = 0
+            }
+    }
+    
+    
+    // Output
+    var body: some View {
+        
+        Image(uiImage: images[index])
+            .resizable()
+            .frame(width: width, height: height, alignment: .center)
+            .gesture(drag)
+            .onAppear{
+                if start <= maxVal && start >= minVal {
+                    index = start
+                }
             }
     }
 }
