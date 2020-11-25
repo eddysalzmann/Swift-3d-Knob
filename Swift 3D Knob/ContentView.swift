8c241b12b716bc50e6077e8ca3cb80f5aa168732
Eddy Salzmann <hello@eddysalzmann.com>
2020-11-25 13:16:42 +0100

    Initial Commit

---
+++ Swift 3D Knob/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift 3D Knob
+//
+//  Created by Eddy Salzmann on 25.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift 3D Knob/Swift_3D_KnobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_3D_KnobApp.swift
+//  Swift 3D Knob
+//
+//  Created by Eddy Salzmann on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_3D_KnobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
